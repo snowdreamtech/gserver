5db884207473f4f2be9f45d0abb17efff083c1ea
snowdream <sn0wdr1am@qq.com>
2025-02-12 02:27:31 +0800

    refactor: gserver env

--- cmd/server/env.go
+++ cmd/server/env.go
@@ -5,8 +5,8 @@ import (
 	"runtime"
 	"strings"
 
-	"github.com/spf13/cobra"
 	"github.com/snowdreamtech/gserver/pkg/env"
+	"github.com/spf13/cobra"
 )
 
 func init() {
@@ -18,17 +18,30 @@ var envCmd = &cobra.Command{
 	Short: "Print " + env.ProjectName + " version and environment info",
 	Long:  "All software has versions. This is " + env.ProjectName + "'s",
 	Run: func(cmd *cobra.Command, args []string) {
-		OSArch := runtime.GOOS + "/" + runtime.GOARCH
-		BuildVersion := fmt.Sprintf("%s version %s %s\n", env.ProjectName, env.GitTag, OSArch)
+		ProjectName := fmt.Sprintf("ProjectName=%s\n", env.ProjectName)
 		GOOS := fmt.Sprintf("GOOS=%s\n", runtime.GOOS)
 		GOARCH := fmt.Sprintf("GOARCH=%s\n", runtime.GOARCH)
 		GOVERSION := fmt.Sprintf("GOVERSION=%s\n", runtime.Version())
+		Author := fmt.Sprintf("Author=%s\n", env.Author)
+		BuildTime := fmt.Sprintf("BuildTime=%s\n", env.BuildTime)
+		GitTag := fmt.Sprintf("GitTag=%s\n", env.GitTag)
+		CommitHash := fmt.Sprintf("CommitHash=%s\n", env.CommitHash)
+		CommitHashFull := fmt.Sprintf("CommitHashFull=%s\n", env.CommitHashFull)
+		COPYRIGHT := fmt.Sprintf("Copyright=%s\n", env.COPYRIGHT)
+		LICENSE := fmt.Sprintf("LICENSE=%s\n", env.LICENSE)
 
 		var builder strings.Builder
-		builder.WriteString(BuildVersion)
+		builder.WriteString(ProjectName)
+		builder.WriteString(Author)
+		builder.WriteString(BuildTime)
+		builder.WriteString(GitTag)
+		builder.WriteString(CommitHash)
+		builder.WriteString(CommitHashFull)
 		builder.WriteString(GOOS)
 		builder.WriteString(GOARCH)
 		builder.WriteString(GOVERSION)
+		builder.WriteString(COPYRIGHT)
+		builder.WriteString(LICENSE)
 
 		fmt.Println(builder.String())
 	},
